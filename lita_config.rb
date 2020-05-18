a27c61e8bac1544eb1ccf41d2a972498954a83f5
aulbytj <aulbourn@gmail.com>
2020-05-18 14:33:21 -0400

    Update lita_config.rb

--- lita_config.rb
+++ lita_config.rb
@@ -17,11 +17,11 @@ Lita.configure do |config|
 
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
-  # config.robot.adapter = :shell
+  config.robot.adapter = :shell
 
   ## Example: Set options for the chosen adapter.
   # config.adapter.username = "myname"
-  config.adapter.password = "secret"
+  # config.adapter.password = "secret"
 
   ## Example: Set options for the Redis connection.
   # config.redis.host = "127.0.0.1"
